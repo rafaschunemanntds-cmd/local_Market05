ed703cbd303487a0bf347a04e02e122a37a90718
rafaschunemanntds-cmd <rafaschunemanntds@gmail.com>
2026-06-18 09:26:57 -0300

    Add files via upload

---
+++ src/Loja/script.js
@@ -0,0 +1,7 @@
+let produtos
+
+window.onload = function(){
+    var storedUser = localStorage.getItem("usuario")
+    var user = JSON.parse(storedUser)
+    var dataEntrada = new Date(user.dataEntrada)
+}
